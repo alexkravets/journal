d6bf95f641daff9c871f7951ff97283b94d0fe0e
Alexander Kravets <a@kra.vc>
2015-10-26 12:25:50 +0200

    Add archive, hide comments, update index show.

--- app/controllers/journal_posts_controller.rb
+++ app/controllers/journal_posts_controller.rb
@@ -1,7 +1,5 @@
 class JournalPostsController < ApplicationController
-  def index
-    @posts = JournalPost.not_hidden
-  end
+  before_filter :set_all_posts, only: %w(index show)
 
 
   def show
@@ -16,6 +14,10 @@ class JournalPostsController < ApplicationController
 
 
   private
+
+    def set_all_posts
+      @posts = JournalPost.not_hidden
+    end
 
     def post_by_int_id
       int_id = params[:hex].to_i(16)

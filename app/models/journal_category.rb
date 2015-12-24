85a83a4a0988a2c4a98de356fd920a4ee681482a
Alexander Kravets <a@kra.vc>
2015-12-24 18:49:54 +0200

    Add Custom Classes
    Add custom classes for Post, Page, Category using int_id.

--- app/models/journal_category.rb
+++ app/models/journal_category.rb
@@ -1,9 +1,14 @@
 class JournalCategory
   include Ants::Content
   include Ants::Orderable
+  include Mongoid::Autoinc
 
   ## Attributes
   field :template_name, default: "show"
+
+  ## Autoinc
+  field :int_id, type: Integer
+  increments :int_id
 
   ## Relations
   has_and_belongs_to_many :posts, class_name: "JournalPost", inverse_of: :categories, index: true
--- app/models/journal_page.rb
+++ app/models/journal_page.rb
@@ -1,7 +1,12 @@
 class JournalPage
   include Ants::Content
   include Ants::Orderable
+  include Mongoid::Autoinc
 
   ## Attributes
   field :template_name, default: "show"
+
+  ## Autoinc
+  field :int_id, type: Integer
+  increments :int_id
 end
